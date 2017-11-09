639a2b7df2f62e760359a3c6e9e6e2c0c7259a47
pdog <pdog@qq.com>
2017-11-09 11:18:13 +0800

    add bezieractivity

--- app/src/main/java/com/example/a18/path/bezier/BezierActivity.java
+++ app/src/main/java/com/example/a18/path/bezier/BezierActivity.java
@@ -1,15 +1,19 @@
 package com.example.a18.path.bezier;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.a18.path.R;
 
-public class BezierActivity extends AppCompatActivity {
+public class BezierActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_bezier2);
+
     }
 }
