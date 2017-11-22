4ecc9819f9c87b5f62cc970d59b2569dadd05af1
pdog <Gj565656>
2017-11-22 10:23:24 +0800

    update kline

--- app/src/main/java/com/example/a18/path/znfxchart/ZNFXChart.java
+++ app/src/main/java/com/example/a18/path/znfxchart/ZNFXChart.java
@@ -8,9 +8,10 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
-import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 
@@ -18,47 +19,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by machao on 2017/11/20.
+ * 智能分析图表
  */
-
 public class ZNFXChart extends View {
+    private static final String TAG = "ZNFXChart";
 
     boolean DEBUG = true;
+    List<KLine> kLines = new ArrayList<>();
 
-
-    List<Path> paths = new ArrayList<>();
-    List<Paint> paints = new ArrayList<>();
     Paint baseLinePaint ;
-    RectF clipRect ;
-    float[][] floats;
-
-    int lineLength = 3;
-
-
-    //峰谷差值 ，默认为控件高度
-    float diffHeight;
-    float diffWidth;
+    RectF clipRect= new RectF();
 
     float peak; // 峰
     float valley;   //谷
 
+
+    boolean pathCreated =false;
     ObjectAnimator animator = ObjectAnimator.ofFloat(this,"progress",1);
 
     public ZNFXChart(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initPaint();
-        floats = new float[lineLength][];
-    }
-
-
-    private void initPaint() {
-        for (int i = 0; i < lineLength; i++) {
-            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            paint.setStrokeWidth(1);
-            paint.setStyle(Paint.Style.STROKE);
-            paints.add(paint);
-            paths.add(new Path());
-        }
+        setLayerType(LAYER_TYPE_SOFTWARE,null);//关闭硬件加速
 
         baseLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         baseLinePaint.setStrokeWidth(2);
@@ -66,88 +47,69 @@ public class ZNFXChart extends View {
         baseLinePaint.setStyle(Paint.Style.STROKE);
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
+    /**
+     * 创建图表的线
+     */
+    private void createKLinePath(){
+        if (getWidth() ==0){
+            Log.e(TAG, "createKLinePath: getWidth == 0 " );
+            return;
+        }
+        for (KLine kLine : kLines) {
+            Path path = kLine.path;
+            float startY = lineHeight(kLine.points[0]);
 
-        //记录图表宽高
-        diffHeight = h;
-        diffWidth = w;
-
-        clipRect = new RectF(0,0,0,h);
-
-
-        setPaths();
-    }
-
-    private void setPaths() {
-
-        for (int i = 0; i < floats.length; i++) {
-            if (floats[i] ==null) {
-                return;
-            }
-
-            Path path = paths.get(i);
-
-            path.reset();
-
-
-            float y1 = lineHeight(floats[i][0]);
-
-            path.moveTo(0, y1);
-
-            for (int j = 1; j < floats[i].length; j++ ) {
-                //算出坐标实际所在的坐标
-                float y2 = lineHeight(floats[i][j]);
-                float x2 = diffWidth/floats[i].length * j ;
+            path.moveTo(0,startY);
+            for (int i = 1; i < kLine.points.length; i++) {
+                float y2 = lineHeight(kLine.points[i]);
+                float x2 = getWidth()/kLine.points.length * i ;
                 path.lineTo(x2, y2);
             }
+            kLine.path  = path;
         }
+        Log.e(TAG, "createKLinePath: 创建 Path ,创建数量" + kLines.size());
+        pathCreated = true;
     }
 
     public void startAnim(){
-        animator.setDuration(2000);
-        animator.setInterpolator(new LinearInterpolator());
-        animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.setRepeatMode(ValueAnimator.REVERSE);
-        animator.start();
+
+        post(new Runnable() {
+            @Override
+            public void run() {
+
+                if (!pathCreated) {
+                    Log.e(TAG, "startAnim:  Path 未创建" );
+                    createKLinePath();
+                }
+                animator.setDuration(2000);
+                animator.setInterpolator(new LinearInterpolator());
+                animator.setRepeatCount(ValueAnimator.INFINITE);
+                animator.setRepeatMode(ValueAnimator.REVERSE);
+                animator.start();
+            }
+        });
+
     }
 
     private void setProgress(float progress){
-        // FIXME: 2017/11/20 处理动画开始时 clipRect为空的情况
-        if (clipRect == null){
-            return;
-        }
-        clipRect.right = diffWidth * progress;
+        clipRect.right = getWidth() * progress;
+        clipRect.bottom = getHeight();
         invalidate();
     }
 
-
-    public ZNFXChart setPaintColor(int index, @ColorInt int color) {
-        paints.get(index).setColor(color);
-        return this;
-    }
-    public ZNFXChart setPaintWidth(int index, int width) {
-        paints.get(index).setStrokeWidth(width);
-        return this;
-    }
-
-    //提供给外界设置y轴坐标的path
-    public ZNFXChart addPath(int index, float[] points) {
-        floats[index] = points;
-        requestLayout();
+    public ZNFXChart addKLine(@NonNull KLine kLine){
+        kLines.add(kLine);
         return this;
     }
 
     //计算当前点的高度
     private float lineHeight(float point) {
-
         //1. 最高点减去最低点
         float diff = peak - valley;
         //2. 高度的区域占总区域的高度
         float topPadding = peak - point;
         //3. 当前高度所占百分比
-        return topPadding / diff * diffHeight;
+        return topPadding / diff * getHeight();
     }
 
     //设置图表的峰谷
@@ -156,6 +118,7 @@ public class ZNFXChart extends View {
         this.valley = valley;
         return this;
     }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -171,16 +134,15 @@ public class ZNFXChart extends View {
     private void drawChartLine(Canvas canvas) {
         canvas.save();
         canvas.clipRect(clipRect);
-        for (int i = 0; i < lineLength; i++) {
-            canvas.drawPath(paths.get(i), paints.get(i));
+        for (int i = 0; i < kLines.size(); i++) {
+            canvas.drawPath(kLines.get(i).path, kLines.get(i).paint);
         }
         canvas.restore();
     }
 
     private void drawBaseLine(Canvas canvas) {
         for (int i = 0; i < 5; i++) {
-            canvas.drawLine(0,diffHeight/5f*i,diffWidth,diffHeight/5f*i,baseLinePaint);
+            canvas.drawLine(0,getHeight()/5f*i,getWidth(),getHeight()/5f*i,baseLinePaint);
         }
-
     }
 }
--- app/src/main/java/com/example/a18/path/znfxchart/ZnfxActivity.java
+++ app/src/main/java/com/example/a18/path/znfxchart/ZnfxActivity.java
@@ -3,12 +3,12 @@ package com.example.a18.path.znfxchart;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.example.a18.path.R;
 
 public class ZnfxActivity extends AppCompatActivity {
     private static final String TAG = "ZnfxActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,31 +24,30 @@ public class ZnfxActivity extends AppCompatActivity {
         for (int i = 0; i < 30; i++) {
             double random = Math.random();
             floats[i] = (float) (random * 60000 + 26000);
-            Log.e(TAG, "onCreate: " + floats[i] );
         }
         for (int i = 0; i < 30; i++) {
             double random = Math.random();
             floats1[i] = (float) (random * 20000 + 66000);
-            Log.e(TAG, "onCreate: " + floats1[i] );
         }
 
         for (int i = 0; i < 30; i++) {
             double random = Math.random();
             floats2[i] = (float) (random * 40000 + 46000);
-            Log.e(TAG, "onCreate: " + floats2[i] );
         }
 
 
-        znfx.setPeakAndValley(99487,15971)
-                .addPath(0, floats)
-                .addPath(1,floats1)
-                .addPath(2,floats2)
-                .setPaintColor(1,Color.RED)
-                .setPaintWidth(1,10)
-                .setPaintColor(2,Color.BLACK)
-                .setPaintWidth(2,10)
-                .setPaintWidth(0, 10)
-                .setPaintColor(0, Color.GREEN);
+        znfx.setPeakAndValley(99487, 15971)
+                .addKLine(new KLine.Builder(floats)
+                        .setColor(Color.BLUE)
+                        .setWidth(20).build())
+                .addKLine(new KLine.Builder(floats1)
+                        .setWidth(20)
+                        .setColor(Color.GREEN)
+                        .build())
+                .addKLine(new KLine.Builder(floats2)
+                        .setWidth(10)
+                        .setColor(Color.RED)
+                        .build());
 
         znfx.startAnim();
 
