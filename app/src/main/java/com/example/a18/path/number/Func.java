1853e1165c9140f960fe0e20fe3697d9b143c797
pdog <pdog@qq.com>
2018-01-06 15:28:10 +0800

    fix slideback layout

--- app/src/main/java/com/example/a18/path/number/Func.java
+++
@@ -1,9 +0,0 @@
-package com.example.a18.path.number;
-
-/**
- * Created by machao on 2017/11/17.
- */
-
-public interface Func {
-
-}
--- app/src/main/java/com/example/a18/path/slidebacklayout/SlideBackLayout.java
+++ app/src/main/java/com/example/a18/path/slidebacklayout/SlideBackLayout.java
@@ -22,40 +22,51 @@ public class SlideBackLayout extends FrameLayout {
     ViewDragHelper.Callback callback = new Callback() {
         @Override
         public int clampViewPositionHorizontal(View child, int left, int dx) {
-//            Timber.d("dx = %s", dx);
-//            Timber.d("left = %s", left);
-//            final int leftBound = getPaddingLeft();
-//            final int rightBound = getWidth() - child.getWidth();
-//            final int newLeft = Math.min(Math.max(left, leftBound), rightBound);
-//
-//            Timber.d("newLeft = %s", newLeft);
             Timber.d("left = %s", left);
             Timber.d("dx = %s", dx);
-            return left;
+
+            int absLeft = Math.abs(left);
+            int width = child.getWidth();
+
+            float ratio = absLeft * 1.0f / width;
+            float alphaRatio = 1.0f - ratio;
+            if (alphaRatio < 0.3f) {
+                alphaRatio = 0.3f;
+            }
+
+            child.setAlpha(alphaRatio);
+            return Math.min(left, 0);
         }
 
         @Override
         public int getViewHorizontalDragRange(View child) {
-            return child == slide ? child.getWidth() : 0;
+            return child == SlideBackLayout.this.leftChild ? child.getWidth() : 0;
         }
 
         @Override
         public void onViewReleased(View releasedChild, float xvel, float yvel) {
             super.onViewReleased(releasedChild, xvel, yvel);
-            Timber.d("releasedChild.getLeft() = %s", releasedChild.getLeft());
-
-            if (releasedChild.getLeft() < releasedChild.getWidth()) {
-                Toast.makeText(releasedChild.getContext(), "<", Toast.LENGTH_SHORT).show();
-                viewDragHelper.settleCapturedViewAt(autoBackViewOriginLeft, autoBackViewOriginTop);
+            Timber.d("xvel = %s", xvel);
+            if (releasedChild.getRight() >= releasedChild.getWidth() * 0.8f) {
+                Toast.makeText(releasedChild.getContext(), "callback", Toast.LENGTH_SHORT).show();
+                releasedChild.animate()
+                    .alpha(0)
+                    .setDuration(500)
+                    .withEndAction(() -> {
+                        layoutLeftChild(releasedChild);
+                        releasedChild.setAlpha(1);
+                    })
+                    .start();
+            }else {
+                Toast.makeText(releasedChild.getContext(), "return", Toast.LENGTH_SHORT).show();
+                viewDragHelper.settleCapturedViewAt(-releasedChild.getWidth(), releasedChild.getTop());
                 invalidate();
-            } else {
-                Toast.makeText(releasedChild.getContext(), "> or =", Toast.LENGTH_SHORT).show();
             }
         }
 
         @Override
         public void onEdgeDragStarted(int edgeFlags, int pointerId) {
-            viewDragHelper.captureChildView(slide, pointerId);
+            viewDragHelper.captureChildView(leftChild, pointerId);
         }
 
         @Override
@@ -63,9 +74,9 @@ public class SlideBackLayout extends FrameLayout {
             return false;
         }
     };
-    private View slide;
     private int autoBackViewOriginLeft;
     private int autoBackViewOriginTop;
+    private AppCompatImageView leftChild;
 
     public SlideBackLayout(Context context) {
         this(context, null);
@@ -78,22 +89,19 @@ public class SlideBackLayout extends FrameLayout {
     public SlideBackLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
-        viewDragHelper = ViewDragHelper.create(this, 0.5f, callback);
-
+        viewDragHelper = ViewDragHelper.create(this, callback);
         viewDragHelper.setEdgeTrackingEnabled(ViewDragHelper.EDGE_LEFT);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        AppCompatImageView imageView = new AppCompatImageView(getContext());
-        LayoutParams params = new LayoutParams(300, ViewGroup.LayoutParams.MATCH_PARENT);
-        imageView.setLayoutParams(params);
-        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-        imageView.setImageResource(R.mipmap.ic_launcher_round);
-        addView(imageView);
-
-        Timber.d("getChildCount() = %s", getChildCount());
+        leftChild = new AppCompatImageView(getContext());
+        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        leftChild.setLayoutParams(params);
+        leftChild.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        leftChild.setImageResource(R.mipmap.slideback);
+        addView(leftChild);
     }
 
 
@@ -117,38 +125,34 @@ public class SlideBackLayout extends FrameLayout {
         }
 
 
-        View content = getChildAt(0);
-        slide = getChildAt(1);
-
-        if (slide != null) {
-            int width = slide.getMeasuredWidth();
-            int height = slide.getMeasuredHeight();
 
 
-            int slideLeft;
-            int slideRight;
-            int slideTop;
-            int slideBottom;
-
-            slideRight = 0;
-            slideLeft = slideRight - width;
-
-            int parentHeight = getMeasuredHeight();
-            slideTop = parentHeight / 2 - height / 2;
-            slideBottom = parentHeight / 2 + height / 2;
-
-            slide.layout(slideLeft, slideTop, slideRight, slideBottom);
-
-            autoBackViewOriginLeft = slide.getLeft();
-            autoBackViewOriginTop = slide.getTop();
-
-            Timber.d("autoBackViewOriginLeft = %s", autoBackViewOriginLeft);
-            Timber.d("autoBackViewOriginTop = %s", autoBackViewOriginTop);
-
+        if (leftChild != null) {
+            layoutLeftChild(leftChild);
         } else {
             Timber.e("slide == null");
         }
 
+    }
+
+    private void layoutLeftChild(View leftChild) {
+        int width = leftChild.getMeasuredWidth();
+        int height = leftChild.getMeasuredHeight();
+
+
+        int slideLeft;
+        int slideRight;
+        int slideTop;
+        int slideBottom;
+
+        slideRight = 0;
+        slideLeft = slideRight - width;
+
+        int parentHeight = getMeasuredHeight();
+        slideTop = parentHeight / 2 - height / 2;
+        slideBottom = parentHeight / 2 + height / 2;
+
+        leftChild.layout(slideLeft, slideTop, slideRight, slideBottom);
     }
 
     @Override
