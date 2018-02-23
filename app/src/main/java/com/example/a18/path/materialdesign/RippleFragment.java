2ce3c2b12b3ce94d319b72f32026f008fc6d661b
pdog <pdog@qq.com>
2018-02-24 00:23:49 +0800

    change traget version

--- app/src/main/java/com/example/a18/path/materialdesign/RippleFragment.java
+++ app/src/main/java/com/example/a18/path/materialdesign/RippleFragment.java
@@ -1,14 +1,24 @@
 package com.example.a18.path.materialdesign;
 
+import android.widget.Toast;
+
 import com.example.a18.path.BaseFragment;
 import com.example.a18.path.R;
+
+import butterknife.OnClick;
 
 
 public class RippleFragment extends BaseFragment {
 
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_gif;
+    }
+
+    @OnClick(R.id.foreground)
+    void foreground() {
+        Toast.makeText(getContext(), "aaaa", Toast.LENGTH_SHORT).show();
     }
 }
 
