3ad5f0643db940ff74fc244c8e70effbf791ce66
pdog <pdog@qq.com>
2018-04-03 19:52:33 +0800

    update to kotlin

--- app/src/main/java/com/example/a18/path/evaluator/EvaluatorActivity.kt
+++ app/src/main/java/com/example/a18/path/evaluator/EvaluatorActivity.kt
@@ -3,28 +3,37 @@ package com.example.a18.path.evaluator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.animation.DecelerateInterpolator
 import android.widget.TextView
-
 import com.example.a18.path.R
 
 class EvaluatorActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
 
-    private var view: TextView? = null
+
+        IncreaseNumber.begin(textView, 999.55566)
+            .apply {
+                leftSize = 64
+                rightSize = 32
+                index = 2
+            }
+            .createAnimator()
+            .setDuration(2000)
+            .apply {
+                interpolator = DecelerateInterpolator(3.0f)
+            }
+            .start()
+    }
+
+    private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_evaluator)
 
-        view = findViewById(R.id.text)
+        textView = findViewById(R.id.text)
         findViewById<View>(R.id.button).setOnClickListener(this)
     }
 
-    override fun onClick(v: View) {
-        IncreaseNumber.begin(view, 9999.55566)
-                .setLeftSize(64)
-                .setRightSize(36)
-                .createAnimator()
-                .setDuration(2000)
-                .start()
-    }
 }
+
--- app/src/main/java/com/example/a18/path/evaluator/IncreaseNumber.kt
+++ app/src/main/java/com/example/a18/path/evaluator/IncreaseNumber.kt
@@ -8,10 +8,10 @@ import android.widget.TextView
 
 
 class IncreaseNumber(private val textView: TextView, private val endValue: Double) {
-    private var index = 2
-    private var leftSize = 16
-    private var rightSize = 16
-    private var convert: Convert? = null
+    var index = 2
+    var leftSize = 16
+    var rightSize = 16
+    var convert: Convert? = null
 
     fun createAnimator(): ValueAnimator {
         val animator = ObjectAnimator.ofObject(IncreaseEvaluator(index), IncreaseEvaluator.IncreaseText(endValue))
@@ -30,25 +30,6 @@ class IncreaseNumber(private val textView: TextView, private val endValue: Doubl
         fun convert(raw: SpannableStringBuilder): SpannableStringBuilder
     }
 
-    fun setConvert(convert: Convert): IncreaseNumber {
-        this.convert = convert
-        return this
-    }
-
-    fun setIndex(index: Int): IncreaseNumber {
-        this.index = index
-        return this
-    }
-
-    fun setLeftSize(leftSize: Int): IncreaseNumber {
-        this.leftSize = leftSize
-        return this
-    }
-
-    fun setRightSize(rightSize: Int): IncreaseNumber {
-        this.rightSize = rightSize
-        return this
-    }
 
     companion object {
 
