3de812ba343126c794793b3282e880e3d521ad42
pdog <pdog@qq.com>
2018-05-03 10:39:05 +0800

    home card

---
+++ module_homecard/src/main/java/kt/pdog18/com/module_homecard/HomeCardActivity.kt
@@ -0,0 +1,12 @@
+package kt.pdog18.com.module_homecard
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class HomeCardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ module_hook/src/main/java/kt/pdog18/com/module_hook/BActivity.kt
@@ -0,0 +1,12 @@
+package kt.pdog18.com.module_hook
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class BActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
+    }
+}
