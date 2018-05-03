77df0f63fc9c59d8b11485a8fc1012b3c6c38b9f
pdog <pdog@qq.com>
2018-05-03 11:15:59 +0800

    f

--- module_homecard/src/main/java/kt/pdog18/com/module_homecard/HomeCardActivity.kt
+++ module_homecard/src/main/java/kt/pdog18/com/module_homecard/HomeCardActivity.kt
@@ -2,11 +2,25 @@ package kt.pdog18.com.module_homecard
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.PagerSnapHelper
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class HomeCardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var adapter = RecyclerAdapter(R.layout.adapter_card_search)
+        adapter.setNewData(listOf(1,2,3,4,5,6,7,8,9,0))
+
+
+        recyclerview.adapter = adapter
+        recyclerview.layoutManager = LinearLayoutManager(this, LinearLayout.HORIZONTAL,false)
+        PagerSnapHelper().attachToRecyclerView(recyclerview)
     }
 }
---
+++ module_homecard/src/main/java/kt/pdog18/com/module_homecard/RecyclerAdapter.java
@@ -0,0 +1,16 @@
+package kt.pdog18.com.module_homecard;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+
+public class RecyclerAdapter extends BaseQuickAdapter<Object,BaseViewHolder>{
+    public RecyclerAdapter(int layoutResId) {
+        super(layoutResId);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Object item) {
+
+    }
+}
