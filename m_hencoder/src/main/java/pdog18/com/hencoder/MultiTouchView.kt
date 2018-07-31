d421a539e915d13b5cd2fa52877da82779b467a7
pdog <pdog@qq.com>
2018-07-31 15:37:37 +0800

    multitouchview

---
+++ m_hencoder/src/main/java/pdog18/com/hencoder/MultiTouchView.kt
@@ -0,0 +1,41 @@
+package pdog18.com.hencoder
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import pdog18.com.core.ext.dp
+
+class MultiTouchView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    val bitmap = getBitmap(resources, R.mipmap.xx, 260.dp)
+
+    var offsetX = 0f
+    var offsetY = 0f
+
+    var downX = 0f
+    var downY = 0f
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                downX = event.x - offsetX
+                downY = event.y - offsetY
+            }
+            MotionEvent.ACTION_MOVE -> {
+                offsetX = event.x - downX
+                offsetY = event.y - downY
+
+                invalidate()
+            }
+        }
+
+        return true
+    }
+
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(bitmap, offsetX, offsetY, null)
+    }
+}
